a3db1ab8283c8db526aadcf07da901bcabbfdfff
shivanieranjikal <eranjikal.s@gmail.com>
2022-03-20 17:17:19 +0530

    Final Complete

--- src/app/recipedata.ts
+++ src/app/recipedata.ts
@@ -4,7 +4,7 @@ export const recipedata: recipeinterface[] = [
     {
         id: 1,
         name: "Chicken Biryani",
-        description: "One Pot Chicken Biryani recipe!",
+        description: "This stunning and warming chicken biryani with creamy coriander and mint sauce recipe is a fabulous dinner idea to serve to the whole family during the winter months.",
         image: "https://geekrobocook.com/wp-content/uploads/2021/03/Tandoori-chicken-biryani.jpg",
         ingredients: [
             {
@@ -20,25 +20,53 @@ export const recipedata: recipeinterface[] = [
                 units: "gm",
                 quantity: 500,
                 price: 0.06
+            },
+            {
+                id: 3,
+                name: "Masala",
+                units: "gm",
+                quantity: 100,
+                price: 0.06
+            },
+            {
+                id: 4,
+                name: "Tomato",
+                units: "gm",
+                quantity: 200,
+                price: 0.06
             }
         ]
     },
     {
         id: 2,
-        name: "Chicken Noodles",
-        description: "Check out the recipe for noodles!",
-        image: "https://hellskitchenrecipes.com/wp-content/uploads/2020/01/Chicken-Stir-Fry-with-Rice-Noodles.png",
+        name: "Egg Noodles",
+        description: "This easy Tea Eggs recipe served with Asian noodles is a wonderful dish to celebrate Chinese New Year. Plus these tea eggs make for a show stopping dish to serve at your next dinner party.",
+        image: "https://media.nbclosangeles.com/2020/10/5003337360-1080pnbcstations.jpg?quality=85&strip=all",
         ingredients: [
             {
-                id: 3,
-                name: "Chicken",
+                id: 5,
+                name: "Egg",
                 units: "gm",
-                quantity: 1000,
-                price: 0.2
+                quantity: 400,
+                price: 0.1
             },
             {
-                id: 4,
+                id: 6,
                 name: "Noodles",
+                units: "gm",
+                quantity: 500,
+                price: 0.1
+            },
+            {
+                id: 7,
+                name: "Dark Soy",
+                units: "gm",
+                quantity: 500,
+                price: 0.1
+            },
+            {
+                id: 8,
+                name: "Vinegar",
                 units: "gm",
                 quantity: 500,
                 price: 0.1
@@ -47,31 +75,45 @@ export const recipedata: recipeinterface[] = [
     },
     {
         id: 3,
-        name: "Momo",
-        description: "Check out the recipe for momos!",
+        name: "Momos",
+        description: "Momos, a steamed or fried dumpling stuffed with mixed vegetables, is a delicious snack food enjoyed in Tibet region. Momos are getting very popular in other parts of India and you can easily prepare them at home.",
         image: "https://www.foodieist.com/wp-content/uploads/2020/12/veg-momos-recipe-672x372.jpg",
         ingredients: [
             {
-                id: 5,
-                name: "Chicken",
+                id: 9,
+                name: "Steamed Chicken",
                 units: "gm",
                 quantity: 1000,
                 price: 0.2
             },
             {
-                id: 6,
+                id: 10,
                 name: "Flour",
                 units: "gm",
                 quantity: 300,
-                price: 0.07
+                price: 0.06
+            },
+            {
+                id: 11,
+                name: "Soya sauce",
+                units: "gm",
+                quantity: 500,
+                price: 0.1
+            },
+            {
+                id: 12,
+                name: "Baking powder",
+                units: "gm",
+                quantity: 500,
+                price: 0.1
             }
         ]
     },
     {
         id: 4,
         name: "Butter Garlic Prawn",
-        description: "Check out the recipe for prawns!",
-        image: "https://manzaii.com/assets/img/items/1609839749Rst6gUTOUm.jpg",
+        description: "This BBQ seafood platter with lemon and garlic butter would be a welcome sight at any Australian Christmas. Try this recipe with your favourite seafood.",
+        image: "http://www.flavorsofmumbai.com/wp-content/uploads/2011/06/buttery-Butter-Garlic-Prawns-1280x720.jpg",
         ingredients: [
             {
                 id: 7,
@@ -86,6 +128,20 @@ export const recipedata: recipeinterface[] = [
                 units: "gm",
                 quantity: 200,
                 price: 0.75
+            },
+            {
+                id: 11,
+                name: "Lemon",
+                units: "gm",
+                quantity: 500,
+                price: 0.1
+            },
+            {
+                id: 12,
+                name: "Pepper",
+                units: "gm",
+                quantity: 500,
+                price: 0.1
             }
         ]
     }
